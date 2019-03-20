f8f07462cf51e15183bf0e3359a7075edc49e4b5
amygemme81 <domathlikearockstar@gmail.com>
2019-03-19 23:21:48 -0700

    I got the spotify data coming in and displaying what I want assuming the user inputs the correct command to call spotify.  If the user does not enter a song track then it will default to a paticular song.

---
+++ keys.js
@@ -0,0 +1,7 @@
+
+console.log('this is loaded');
+
+exports.spotify = {
+    id: "288adfa9531e4224bd3fd5e770072f54",
+    secret: "20cfebf72a824037bb0887bd9bd958b6"
+};
--- liri.js
+++ liri.js
@@ -1,36 +1,93 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+
+
+var spotify = new Spotify(keys.spotify);
+
+// var inquirer = require('inquirer');
+// inquirer
+//   .prompt([
+//     {
+//       type: "checkbox",
+//       radio: "yes",
+//       name: "choice",
+//       message: "What would you like to do?",
+//       choices: ["concert-this","spotify-this-song","movie-this", "do-what-it-says"]
+//     },
+//   ])
+//   .then(answers => {
+//     console.log(answers);
+//   });
+
+// ============ Movie Code =============// 
+
+if (process.argv[2] === "movie-this") {
 var fullname = "Matrix";
 
 var axios = require("axios");
 
 axios.get("http://www.omdbapi.com/?t=" + fullname + "&y=&plot=short&apikey=337d7ea4").then(
-  function(response) {
-      console.log(response);
-})
+  function (response) {
+   console.log(response.data.Title);
+   console.log(response.data.Year);
+   console.log(response.data.imdbRating);
+   console.log(response.data.Language);
+   console.log(response.data.Country);
+   //  I am not sure where to get the rotten tomatoes rating data (skipping for now!)
+   //console.log(response.data.Ratings);
+    
 
 
-
-// PAste code here
-
-var Spotify = require('node-spotify-api');
- 
-var spotify = new Spotify({
-  id: "288adfa9531e4224bd3fd5e770072f54",
-  secret: "20cfebf72a824037bb0887bd9bd958b6"
-});
- 
-// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-//   if (err) {
-//     return console.log('Error occurred: ' + err);
-//   }
- 
-// console.log(data); 
-// });
-
-spotify
-  .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-  .then(function(data) {
-    console.log(data); 
   })
-  .catch(function(err) {
-    console.error('Error occurred: ' + err); 
+}
+
+
+// ======== Spotify Code =========== //
+
+if (process.argv[2] === "spotify-this-song") 
+{
+  if (process.argv[3] === undefined) {
+    spotify.search({ type: 'track', query: "The Sign Ace of Base", limit: 1 }, function (err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+    
+      let items = data.tracks.items[0];
+      let artist = items.artists[0];
+      let album = items.album;
+      console.log("artist: " + artist.name);
+      console.log("Album: " + album.name);
+      console.log("link to track: " + items.external_urls.spotify);
+      console.log("Track: " + items.name);
+
+          });
+  }
+  else{
+    let song = process.argv[3];
+    spotify.search({ type: 'track', query: song, limit: 1 }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    let items = data.tracks.items[0];
+    let artist = items.artists[0];
+    let album = items.album;
+    console.log("artist: " + artist.name);
+    console.log("Album: " + album.name);
+    console.log("link to track: " + items.external_urls.spotify);
+    console.log("Track: " + items.name);
+  
   });
+}
+}
+ //spotify.search = function({ type: 'artist OR album OR track', query: 'rebel yell', limit: 2 }, callback)
+
+
+// spotify
+//   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+//   .then(function(data) {
+//     console.log(data); 
+//   })
+//   .catch(function(err) {
+//     console.error('Error occurred: ' + err); 
+// 
